5addb4ff1004d2aa9243f8745a7854b09af0f57c
melihozmen9 <105754659+melihozmen9@users.noreply.github.com>
2022-06-19 02:36:33 +0300

    properties2

--- properties/ViewController.swift
+++ properties/ViewController.swift
@@ -19,6 +19,10 @@ class ViewController: UIViewController {
         deneme.age = 26
         print(deneme.age)
         
+        //
+        var user = user()
+        user.user2.age = 27
+        print(user.user2.age)
     }
 
 
@@ -27,4 +31,18 @@ class ViewController: UIViewController {
 struct userinfo {
     let name:String // let ile tanımlandığı için sabit bir değer ve değiştirilemez.
     var age:Int // var ile tanımlandığı için değeri değiştirilebilir.
+}
+struct userinfo2 {
+    var name: String
+    var age: Int
+    
+    init (_name:String,_age:Int){
+    print("init çalıştı")
+        self.name = _name
+        self.age = _age
+    }
+}
+struct user {
+   // var user1 = userinfo2(_name: "melih", _age: 25)
+   lazy var user2 = userinfo2(_name: "melih", _age: 25)
 }
